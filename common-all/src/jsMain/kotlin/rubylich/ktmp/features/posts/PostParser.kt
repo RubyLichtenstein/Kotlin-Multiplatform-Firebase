4848300c07324bc2b7c8c92699377c0da454ec50
rl98880 <ruby.lichtenstein@citi.com>
2018-10-31 22:10:53 +0200

    fixed firebase deployment problem

--- common-all/src/jsMain/kotlin/rubylich/ktmp/features/posts/PostParser.kt
+++ common-all/src/jsMain/kotlin/rubylich/ktmp/features/posts/PostParser.kt
@@ -1,15 +1,20 @@
 package rubylich.ktmp.features.posts
 
-import kotlinx.serialization.DynamicObjectParser
-import kotlinx.serialization.ImplicitReflectionSerializer
 import rubylich.ktmp.functions.ts2kt_firebase_admin.DocumentSnapshot
 import rubylich.ktmp.repo.Parser
 
 actual class PostParser actual constructor() : Parser<Post> {
+//    @ImplicitReflectionSerializer
     override fun parse(any: Any): Post {
-        return (any as DocumentSnapshot).parse()
+        val snapshot = (any as DocumentSnapshot)
+        return Post(
+            id = snapshot.get("id") as String,
+            content = snapshot.get("content") as String
+        )
+//        return (any as DocumentSnapshot).parse()
     }
 }
-
-@UseExperimental(ImplicitReflectionSerializer::class)
-inline fun <reified T: Any> DocumentSnapshot.parse(): T = DynamicObjectParser().parse(this.data().asDynamic())
+//do to firebase functions deployment bug
+//Error: Error occurred while parsing your function triggers. Please ensure that index.js only exports cloud functions.
+//@UseExperimental(ImplicitReflectionSerializer::class)
+//inline fun <reified T: Any> DocumentSnapshot.parse(): T = DynamicObjectParser().parse(this.data().asDynamic())
