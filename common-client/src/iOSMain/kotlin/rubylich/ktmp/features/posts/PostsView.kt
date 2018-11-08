4a961a9ebda0e2e2498f4cd88e3b38271b563990
rl98880 <ruby.lichtenstein@citi.com>
2018-11-08 20:27:01 +0200

    native coro

--- common-client/src/iOSMain/kotlin/rubylich/ktmp/features/posts/PostsView.kt
+++ common-client/src/iOSMain/kotlin/rubylich/ktmp/features/posts/PostsView.kt
@@ -1,11 +1,12 @@
 package rubylich.ktmp.features.posts
 
-import kotlin.coroutines.coroutineContext
+import kotlinx.channels.SendChannel
+import kotlinx.coroutines.channels.BroadcastChannel
 
 actual class PostsView : IPostsView {
     actual override fun showPosts(posts: List<Post>) {}
     actual override fun showAddPostDialog() {}
-    actual override fun addPostButtonClick(): BroadcastChannel<Unit> {
+    actual override fun addPostButtonClick(): SendChannel<Unit> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
@@ -15,7 +16,6 @@ actual class PostsView : IPostsView {
 
     actual override fun showError(error: Throwable) {}
     actual override fun refresh(): BroadcastChannel<Unit> {
-        coroutineContext()
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
