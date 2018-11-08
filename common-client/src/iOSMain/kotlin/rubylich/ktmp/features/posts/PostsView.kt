9e32727a8a594841f8dae049df79339f96160fca
ornoyman <or.noyman@citi.com>
2018-11-08 21:00:57 +0200

    fix return type

--- common-client/src/iOSMain/kotlin/rubylich/ktmp/features/posts/PostsView.kt
+++ common-client/src/iOSMain/kotlin/rubylich/ktmp/features/posts/PostsView.kt
@@ -1,12 +1,11 @@
 package rubylich.ktmp.features.posts
 
-import kotlinx.channels.SendChannel
 import kotlinx.coroutines.channels.BroadcastChannel
 
 actual class PostsView : IPostsView {
     actual override fun showPosts(posts: List<Post>) {}
     actual override fun showAddPostDialog() {}
-    actual override fun addPostButtonClick(): SendChannel<Unit> {
+    actual override fun addPostButtonClick(): BroadcastChannel<Unit> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
