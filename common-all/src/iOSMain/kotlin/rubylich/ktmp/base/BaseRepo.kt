023f06176fbae51f9d7c0b8464c54fe7248fb8f1
ornoyman <or.noyman@citi.com>
2018-11-14 10:48:31 +0200

    iOS repository fixes (work env is sane again)

--- common-all/src/iOSMain/kotlin/rubylich/ktmp/base/BaseRepo.kt
+++ common-all/src/iOSMain/kotlin/rubylich/ktmp/base/BaseRepo.kt
@@ -1,11 +1,7 @@
 package rubylich.ktmp.base
 
 import com.firebase.firestore.FIRFirestore
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.GlobalScope
 import rubylich.ktmp.awaitCallback
-import kotlinx.serialization.Mapper
 
 actual abstract class BaseRepo<T : Any> actual constructor(
         ref: String,
@@ -25,7 +21,7 @@ actual abstract class BaseRepo<T : Any> actual constructor(
                 if (error != null) {
                     cont.onError(Exception(error.localizedDescription))
                 } else {
-                    cont.onComplete(parser.parse(document!!))
+                    cont.onComplete(document!!.documents.filterNotNull().map { parser.parse(it) })
                 }
             }
         }
@@ -54,9 +50,13 @@ actual abstract class BaseRepo<T : Any> actual constructor(
     }
 
     actual override suspend fun delete(id: String) {
-        return awaitCallback {
+        return awaitCallback { cont ->
             collection.documentWithPath(id).deleteDocumentWithCompletion { error ->
-                cont.onError(Exception(error))
+                if (error != null) {
+                    cont.onError(Exception(error.localizedDescription))
+                } else {
+                    cont.onComplete(Unit)
+                }
             }
         }
     }
@@ -64,7 +64,11 @@ actual abstract class BaseRepo<T : Any> actual constructor(
     actual override suspend fun update(id: String, field: String, value: Any) {
         return awaitCallback { cont ->
             collection.documentWithPath(id).updateData(mapOf(field to value)) { error ->
-                cont.onError(Exception(error))
+                if (error != null) {
+                    cont.onError(Exception(error.localizedDescription))
+                } else {
+                    cont.onComplete(Unit)
+                }
             }
         }
     }
